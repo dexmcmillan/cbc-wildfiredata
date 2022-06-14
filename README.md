be5b56101399363c805fb95018b0b9f6eeee5a8b
dexmcmillan <dexmcmillan@gmail.com>
2022-06-14 15:18:21 -0400

    added analysis file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wildfire data scraper
+
+This repo will scrape active wildfire data from NRCAN's server every 2 hours.
---
+++ analysis.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import datawrappergraphics
+import glob
+
+csvfiles = glob.glob("data/*.csv")
+
+dfs = []
+
+for file in csvfiles:
+    df = pd.read_csv(file)
+    
+    dfs.append(df)
+    
+data = pd.concat(dfs)
+
+data = data.loc[~data["agency"].isin(["ak", "conus", "pc"]), :]
+
+data["stage_of_control"] = data["stage_of_control"].str.strip().replace({
+    "OC": "Out of control",
+    "UC": "Under control",
+    "EX": "Extinguished",
+    "BH": "Being held"
+})
+
+data["province/territory"] = data["agency"].replace({
+    "sk": "Saskatchewan",
+    "ab": "Alberta",
+    "bc": "British Columbia",
+    "on": "Ontario",
+    "ns": "Nova Scotia",
+    "yt": "Yukon",
+    "nt": "Northwest Territories",
+    "qc": "Quebec",
+    "mb": "Manitoba",
+    "nl": "Newfoundland and Labrador",
+    "pe": "Prince Edward Island"
+})
+
+pivot = data.pivot(columns="firename", index="timestamp", values="hectares")
+# pivot = round(pivot.pct_change()*100, 2)
+
+chart = datawrappergraphics.Chart("0XCbi").data(pivot)
+
+meta_obj = {k: "#cccccc" for k in pivot.columns}
+
+chart.metadata["metadata"]["visualize"]["custom-colors"] = meta_obj
+
+chart.set_metadata()
