860b884013d2ced76d012d634c7d86e21265ba75
dexmcmillan <dexmcmillan@gmail.com>
2023-05-08 11:19:24 -0400

    removed chart update script from sked

--- scrape-bc.py
+++ scrape-bc.py
@@ -4,7 +4,7 @@ import requests
 
 active_fires_bc = "https://services6.arcgis.com/ubm4tcTYICKBpist/arcgis/rest/services/BCWS_FirePerimeters_PublicView/FeatureServer/0/query?returnGeometry=true&where=FIRE_STATUS%20%3C%3E%20%27Out%27&outSR=4326&outFields=*&inSR=4326&geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&geometryPrecision=6&resultType=tile&f=geojson"
 
-data = geopandas.read_file(active_fires_bc)
+data = geopandas.read_file(active_fires_bc, engine="fiona")
 
 timestamp = pd.to_datetime(requests.get(active_fires_bc).headers["Last-Modified"])
 data["FIRE_NUMBER"] = timestamp
