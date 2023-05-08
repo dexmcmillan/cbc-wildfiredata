2d6d6464e5f33382889e25206e40a8e886c40645
dexmcmillan <dexmcmillan@gmail.com>
2023-05-08 11:17:37 -0400

    tweaks to geopandas read in

--- scrape-ab.py
+++ scrape-ab.py
@@ -4,9 +4,8 @@ import requests
 
 active_fires_ab = "https://services.arcgis.com/Eb8P5h4CJk8utIBz/arcgis/rest/services/Active_Wildfire_Perimeters_Simplified_view/FeatureServer/0/query?f=json&resultOffset=0&resultRecordCount=4000&where=1=1&orderByFields=OBJECTID&outFields=*&resultType=tile&spatialRel=esriSpatialRelIntersects&geometryType=esriGeometryEnvelope&defaultSR=102100"
 
-data = geopandas.read_file(active_fires_ab)
+data = geopandas.read_file(active_fires_ab, engine="fiona")
 
-print(data)
 timestamp = pd.to_datetime(requests.get(active_fires_ab).headers["Last-Modified"])
 data["FIRENUMBER"] = timestamp
 
