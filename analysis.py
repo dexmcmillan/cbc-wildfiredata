137de9881d6e69e7b3ff18c71b20338703ec62bc
Dexter McMillan <dexmcmillan@gmail.com>
2023-06-07 10:50:20 -0400

    updating

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datawrappergraphics
 import glob
 import datetime as dt
+import os
 
 csvfiles = glob.glob("data/*.csv")
 
@@ -14,9 +15,7 @@ for file in csvfiles:
     
 data = pd.concat(dfs)
 
-data["timestamp"] = pd.to_datetime(data["timestamp"]).dt.tz_convert(None)
-
-print(data)
+data["timestamp"] = pd.to_datetime(data["timestamp"])
 
 data = data.loc[~data["agency"].isin(["ak", "conus", "pc"]), :]
 
@@ -50,7 +49,14 @@ data["color"] = data["stage_of_control"].replace({
     "Being held": "#29414F"
 })
 
-out_of_control = len(data.loc[data["stage_of_control"] == "Out of control", "firename"].unique())
+most_recent_file = pd.read_csv(max(csvfiles, key=os.path.getctime))
+
+try:
+    active_fires_list = most_recent_file.loc[(data["stage_of_control"] != "OC"), "firename"]
+except IndexError:
+    active_fires_list = []
+
+out_of_control = len(data.loc[data["firename"].isin(active_fires_list), "firename"].unique())
 
 pivot = data.pivot(columns="firename", index="timestamp", values="hectares")
 # pivot = round(pivot.pct_change()*100, 2)
@@ -59,9 +65,7 @@ for label, i in pivot.iteritems():
     if pivot[label].max() == pivot[label].min() or pivot[label].max() <= 100:
         pivot = pivot.drop(columns=label)
 
-
-
-chart = datawrappergraphics.Chart("0XCbi").data(pivot).head(f"Wildfires in Canada since June 14").deck(f"There are <b>{out_of_control}</b> fires that are classified as <span style='background-color:#C42127; color:white;padding:1px 3px;border-radius:2px'>out of control</span>. Chart shows wildfires that have increased or decreased since June 14, and are currently larger than 100 hectares.")
+chart = datawrappergraphics.Chart("0XCbi").data(pivot).head(f"Wildfires in Canada since June 14").deck(f"There are currently <b>{out_of_control}</b> fires that are classified as <span style='background-color:#C42127; color:white;padding:1px 3px;border-radius:2px'>out of control</span>. Chart shows wildfires that have increased or decreased since June 14, and are currently larger than 100 hectares.")
 
 meta_obj = {k: data.set_index("firename").at[k,"color"].max() for k in pivot.columns}
 
